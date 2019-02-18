5f7a54d183cb47718a3b34beb8e9476fb97e5f37
jinsoo <sjins0127@gmail.com>
2019-02-18 17:29:46 +0900

    dddd

--- swex/src/swex/Solution_2819.java
+++ swex/src/swex/Solution_2819.java
@@ -18,6 +18,7 @@ public class Solution_2819 {
 	static int[] s;
 	static Set<String> set;
 	static int arr[][];
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 
 		String src = "1\r\n" + "1 1 1 1\r\n" + "1 1 1 2\r\n" + "1 1 2 1\r\n" + "1 1 1 1";
@@ -34,18 +35,13 @@ public class Solution_2819 {
 					arr[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
-//			s = new int[7];
-//			per(0, 0, 0, 0);
 			for (int i = 0; i < 4; i++) {
 				for (int j = 0; j < 4; j++) {
-//					System.out.println("좌표"+i+" "+j+" "+arr[i][j]);
 					s = new int[7];
 					per(i, j, 0, 0);
 				}
 			}
-			
-			System.out.println(set.size());
-			System.out.println("#" + tc + " ");
+			System.out.println("#" + tc + " " + set.size());
 		}
 	}
 
@@ -53,19 +49,15 @@ public class Solution_2819 {
 		// x,y는 시작 위치
 		// idx는 dxy용!!!!!
 		if (cnt == 7) {
-//			System.out.println(Arrays.toString(s));
 			set.add(Arrays.toString(s));
-//			System.out.println(s.toString());
-//			s.toString();
-//			System.out.println("좌표"+x+" "+y+" ");
 			return;
 		}
 		s[cnt] = arr[x][y];
 		for (int i = 0; i < 4; i++) {
 			// 범위체크하고
 			// 갈수있으면 보내기
-			int cx = x + dxy[i][0]*1;
-			int cy = y + dxy[i][1]*1;
+			int cx = x + dxy[i][0] * 1;
+			int cy = y + dxy[i][1] * 1;
 			if (cx >= 0 && cx < 4 && cy >= 0 && cy < 4) {
 				per(cx, cy, i, cnt + 1);
 			}
