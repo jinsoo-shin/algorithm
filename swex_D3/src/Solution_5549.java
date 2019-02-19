5e7aafbe2c021a06ef3079373459f777e322c004
144167 <144167@DESKTOP-5HE1FCJ>
2019-02-19 20:18:55 +0900

    ddd

--- swex_D3/src/Solution_5549.java
+++ swex_D3/src/Solution_5549.java
@@ -1,11 +1,5 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.StringReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Solution_5549 {
 //	public class Solution {
