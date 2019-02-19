94a6e0252311383af5783a365921151d74160dab
144167 <144167@DESKTOP-5HE1FCJ>
2019-02-19 20:18:38 +0900

    dddd

--- swex_D3/src/Solution_5549.java
+++ swex_D3/src/Solution_5549.java
@@ -1,7 +1,13 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-public class Solution_기본 {
+public class Solution_5549 {
 //	public class Solution {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 //		System.setIn(new FileInputStream("res/input.txt"));
