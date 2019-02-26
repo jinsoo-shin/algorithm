362ad028a4a650c17cd5afeff1379365db361fa2
jinsoo <sjins0127@gmail.com>
2019-02-26 16:22:30 +0900

    d

--- 알고리즘/src/Main_1863.java
+++ 알고리즘/src/Main_1863.java
@@ -1,9 +1,8 @@
 import java.io.*;
 import java.util.*;
 
-public class 서로소집합UnionFind {
+public class Main_1863 {
 
-//d
 //Disjoint-Set 서로소 집합
 //Union-Find 유니온 파인드 => 합집합 찾기
 //
@@ -18,24 +17,24 @@ public class 서로소집합UnionFind {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int V = Integer.parseInt(st.nextToken());// 점 수
 		int E = Integer.parseInt(st.nextToken());// 간선 수
-		int[][] edges = new int[E][2];
-
-		for (int i = 0; i < E; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken()) - 1;
-			int b = Integer.parseInt(st.nextToken()) - 1;
-			edges[i][0] = a;
-			edges[i][1] = b;
-
-		}
+//		int[][] edges = new int[E][2];
 		parents = new int[V];
 		for (int i = 0; i < V; i++) {
 			makeSet(i);// 초기화 하기
 		}
-
 		for (int i = 0; i < E; i++) {
-			Union(edges[i][0], edges[i][1]);
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken()) - 1;
+			int b = Integer.parseInt(st.nextToken()) - 1;
+			union(a, b);
+//			edges[i][0] = a;
+//			edges[i][1] = b;
+
 		}
+
+//		for (int i = 0; i < E; i++) {
+//			union(edges[i][0], edges[i][1]);
+//		}
 		int ans = 0;
 		for (int i = 0; i < V; i++) {
 			if (parents[i] == i) {
@@ -62,7 +61,7 @@ public class 서로소집합UnionFind {
 	}
 
 	// 부모노드를 합치는 함수
-	static void Union(int x, int y) {
+	static void union(int x, int y) {
 		int a = findSet(x);
 		int b = findSet(y);// 각각 부모찾기
 		if (a < b) {
@@ -71,39 +70,4 @@ public class 서로소집합UnionFind {
 			parents[a] = b;
 		}
 	}
-
-//	
-//	static void makeSet(int x) { // 자기자신 집합 만들기
-//		parents[x] = x;
-//	}
-//
-//	static int findSet(int x) {
-//		if (x == parents[x]) {
-//			return x;
-//		} else {
-//			parents[x] = findSet(parents[x]);
-//			return parents[x];
-//		}
-//	}
-//
-//	static void Union(int x, int y) {
-//		// x,y 중 대표자를 찾아서
-//		// 각 두 대표자중 Rank가 높은애를 대표자로
-//		// Rank가 같은경우 대표자가 되는애를 rank++;
-//		int px = findSet(x);
-//		int py = findSet(y);// 부모찾고
-//
-////		if (px != py) {
-////			parents[py] = px;
-////		}
-//		if (rank[px] > rank[py]) {
-//			parents[py] = px;
-//		} else {
-//			parents[px] = py;
-//			if (rank[px] == rank[py]) {
-//				rank[py]++;
-//			}
-//		}
-//
-//	}
 }
