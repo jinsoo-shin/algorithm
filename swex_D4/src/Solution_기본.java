1bda378befb0a9247ef4209c0c471d38e2610d6d
jinsoo <sjins0127@gmail.com>
2019-02-26 16:22:50 +0900

    sdf

---
+++ swex_D4/src/Solution_기본.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution_기본 {
+//	public class Solution {
+	public static void main(String[] args) throws Exception{
+//		System.setIn(new FileInputStream("res/input.txt"));
+//		InputStream input = Solution_기본.class.getResourceAsStream("jump.txt");
+		String src = "";
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		br = new BufferedReader(new StringReader(src));
+		StringTokenizer st;
+
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			int N = Integer.parseInt(br.readLine());
+			int ans = 0;
+			st = new StringTokenizer(br.readLine());
+
+			System.out.println("#" + tc + " " + ans);
+		}
+	}
+}
