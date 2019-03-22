8d290f7c5e80f3ea69ce1df24a6ca0c8189fbb76
신진수 <sjins0127@gmail.com>
2019-03-22 18:00:00 +0900

    231

--- swex/src/Solution_기본.java
+++ swex/src/Solution_기본.java
@@ -4,9 +4,10 @@ import java.util.*;
 public class Solution_기본 {
 //	public class Solution {
 	public static void main(String[] args) throws Exception{
-//		System.setIn(new FileInputStream("res/input.txt"));
-//		InputStream input = Solution_기본.class.getResourceAsStream("jump.txt");
-		String src = "";
+		String src = "3\r\n" + 
+				"3 17 1 39 8 41 2 32 99 2\r\n" + 
+				"22 8 5 123 7 2 63 7 3 46\r\n" + 
+				"6 63 2 3 58 76 21 33 8 1\r\n";
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		br = new BufferedReader(new StringReader(src));
 		StringTokenizer st;
